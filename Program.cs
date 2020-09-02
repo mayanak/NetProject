8edd461334beaf3b4c2619da6277780b4187bffd
mayanak <45207952+mayanak@users.noreply.github.com>
2020-09-03 00:10:58 +0530

    StartupClass
    Start program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentRuleEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactoryPayment payment objPayment =new FactoryPayment();
+        }
+
+        
+    }
+}
